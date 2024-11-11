b5cbad844aefffbe252dd95f8402c88f36265ea6
RohitSingh <thakurrohitdsingh@gmail.com>
2024-11-11 18:46:27 +0530

    formated

--- src/entity/Employee.java
+++ src/entity/Employee.java
@@ -1,6 +1,6 @@
 package entity;
 
-import javax.persistence.Entity;
+import javax.persistence.Entity; 
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
